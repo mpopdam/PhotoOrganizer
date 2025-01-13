f18ec91fdf45b54457bc81208476573dbac4701d
Martin Opdam <martin.opdam@avivasolutions.nl>
2025-01-13 15:37:34 +0100

    Update Nuke to 9.0.3

--- build/Build.cs
+++ build/Build.cs
@@ -1,16 +1,7 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
